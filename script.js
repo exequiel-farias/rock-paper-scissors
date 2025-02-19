185c8838a4c0f00e0d4af2f44ca4994b8f962508
Exequiel Farías <exequiel-farias@outlook.com>
2025-02-19 11:15:08 -0300

    Add logica de jugar una ronda

--- script.js
+++ script.js
@@ -11,9 +11,46 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-  let humainChoice = prompt("Please enter your choice: rock, paper or scissors.");
-  return humainChoice;
+  let humanChoice = prompt("Please enter your choice: rock, paper or scissors.");
+  return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice){
+  let humanChoiceLowerCase = humanChoice.toLowerCase();
+  let computerChoiceLowerCase = computerChoice.toLowerCase();
+  if(humanChoiceLowerCase === computerChoiceLowerCase){
+    return "It's a tie!";
+  }else if(humanChoiceLowerCase === "rock"){
+    if(computerChoiceLowerCase === "paper"){
+      computerScore++;
+      return "You lose! Paper beats Rock.";
+    }else{
+      humanScore++;
+      return "You win! Rock beats Scissors.";
+    }
+  }else if(humanChoiceLowerCase === "paper"){
+    if(computerChoiceLowerCase === "rock"){
+      humanScore++;
+      return "You win! Paper beats Rock.";
+    }else{
+      computerScore++;
+      return "You lose! Scissors beats Paper.";
+    }
+  }else if(humanChoiceLowerCase === "scissors"){
+    if(computerChoiceLowerCase === "rock"){
+      computerScore++;
+      return "You lose! Rock beats Scissors.";
+    }else{
+      humanScore++;
+      return "You win! Scissors beats Paper.";
+    }
+  }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
